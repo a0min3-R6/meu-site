f47a6249cfd5de30286dce8011a38b41b2f0a695
a0min3-R6 <ddanilodesouzasilva@gmail.com>
2023-07-21 21:20:31 -0300

    javascript

---
+++ javascript/variaveis.js
@@ -0,0 +1,14 @@
+console.log(nome)
+// usando var ou nada
+// identificador
+//  atribuição
+// valor
+var nome = "Danilo"
+sobrenome = "Silva"
+console.log(nome)
+
+//não permite a execução antes da criação (ERRO)
+let nome2 = "Senac"
+console.log(nome2)
+
+const pi=3.141923
